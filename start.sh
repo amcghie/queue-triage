6644eaf3c35dae23b70feba0c3b3916468578f6d
Andrew McGhie <andrew.mcghie@gmail.com>
2018-01-11 22:23:56 +1000

    #88: First stab at adding properties popup/dialog

--- start.sh
+++ start.sh
@@ -3,15 +3,15 @@
 case "$1" in
     core)
         buck build //core/server:start-script
-        $(dirname $0)/buck-out/gen/core/server/start-script/start-core.sh &
+        $(dirname $0)/buck-out/gen/core/server/start-script/start-core.sh
         ;;
     ldap)
         buck build //common/ldap-test-support:start-script
-        $(dirname $0)/buck-out/gen/common/ldap-test-support/start-script/start-ldap.sh &
+        $(dirname $0)/buck-out/gen/common/ldap-test-support/start-script/start-ldap.sh
         ;;
     web)
         buck build //web/server:start-script
-        $(dirname $0)/buck-out/gen/web/server/start-script/start-web.sh &
+        $(dirname $0)/buck-out/gen/web/server/start-script/start-web.sh
         ;;
     *)
         echo "Usage: $0 {core|ldap|web}"
